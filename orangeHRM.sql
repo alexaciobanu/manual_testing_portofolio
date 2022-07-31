a4100fde9326f582d041839f959e8aa93d850624
alexaciobanu <107039535+alexaciobanu@users.noreply.github.com>
2022-07-31 17:55:14 +0300

    Update orangeHRM.sql

--- orangeHRM.sql
+++ orangeHRM.sql
@@ -6,7 +6,9 @@ userrole varchar (25) not null,
 employeename varchar (25) not null,
 status varchar (25) not null
 );
+
 select * from users;
+
 insert into users (username, userrole, employeename, status) 
 values ('ananya708', 'Admin', 'Ananya Smith', 'Enabled'),
 ('john92', 'ESS', 'John Samson', 'Disabled'),
@@ -15,16 +17,26 @@ values ('ananya708', 'Admin', 'Ananya Smith', 'Enabled'),
 ('harrykane', 'ESS', 'Harry Kane', 'Enabled'),
 ('jasmine1', 'Admin', 'Jasmine Morgan', 'Disabled'),
 ('nathane', 'ESS', 'Nathan Elliot', 'Enabled');
+
 select * from users;
+
 alter table users add column userid int primary key auto_increment;
+
 select * from users;
+
 alter table users modify column userid int auto_increment first;
+
 select count(*) from users;
+
 select username from users where status='Disabled';
+
 select username, count(*) from users group by status;
+
 update users
+
 set userrole = 'ESS'
 where username = 'nathane';
+
 delete from users where userid = 5;
 
 
